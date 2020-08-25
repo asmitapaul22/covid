94324d6a8f2ea20e7d8629eec348557cdb6e9ae7
asmitapaul22 <asmitapaul2000@gmail.com>
2020-08-25 11:05:30 +0530

    first commit

---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+private globalDataUrl='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-20-2020.csv';
+  constructor(private http: HttpClient) { }
+  getGlobalData(){
+      return this.http.get(this.globalDataUrl).pipe(map(result=>{
+        console.log(result);
+      })) 
+  }
+}
